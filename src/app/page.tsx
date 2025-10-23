594ab62d2554b092b4a660fc3990f7562594cf05
twoMokko <two.mokko@gmail.com>
2025-10-23 13:36:56 -0700

    add swiper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 import styles from './page.module.scss';
 import { Button } from "../shared/ui/button/Button";
+import { ProductCarousel } from "@/src/widgets/product-carousel";
 
 export const metadata: Metadata = {
   title: 'Главная страница',
@@ -8,11 +9,12 @@ export const metadata: Metadata = {
 }
 
 export default function Home() {
-  return (
-      <div className="bg-blue-100 px-20">
-        <h1 className={styles.title}>Главная страница заголовок</h1>
-        <p className="py-10">текст на главной тест</p>
-        <Button variant="primary">Кнопка</Button>
-      </div>
-  );
+    return (
+        <div className="container">
+            <h1 className={styles.title}>Главная страница заголовок</h1>
+            <p>текст на главной тест</p>
+            <ProductCarousel />
+            <Button variant="primary">Кнопка</Button>
+        </div>
+    );
 }
