93a3172b1849778e8153b4a6c685f7ede68ca2f2
twoMokko <two.mokko@gmail.com>
2025-10-27 12:09:49 -0700

    открытие модальных окон в блоке акции

--- src/widgets/actions/ui/Actions.tsx
+++ src/widgets/actions/ui/Actions.tsx
@@ -1,18 +1,29 @@
 'use client'
 
-import styles from './Actions.module.scss'
+import styles from './Actions.module.scss';
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import React from "react";
+import { ModalType, useModal } from "@/src/app/providers/ModalProvider";
 
-const data = [
-    { id: 1, title: 'Автокредит с выгодой до 400 000₽', subtitle: 'Одобрение по 2-ум документам', link: '', icon: '' },
-    { id: 2, title: 'Гарантия 3 года или комплект резины в подарок!', subtitle: 'на любую модель при покупке', link: '', icon: '' },
-    { id: 3, title: 'Выгоды по Трейд-ин до 300 000₽', subtitle: 'Только 20 автомобилей', link: '', icon: '' },
-    { id: 4, title: 'Новые SWM по сниженной цене от 1 395 000₽!', subtitle: 'Только 20 автомобилей', link: '', icon: '' },
+interface dataActions {
+    id: number,
+    title: string,
+    subtitle: string,
+    link: ModalType,
+    icon: string | React.ReactNode
+}
+
+const data: dataActions[]  = [
+    { id: 1, title: 'Автокредит с выгодой до 400 000₽', subtitle: 'Одобрение по 2-ум документам', link: 'credit', icon: '' },
+    { id: 2, title: 'Гарантия 3 года или комплект резины в подарок!', subtitle: 'на любую модель при покупке', link: 'order', icon: '' },
+    { id: 3, title: 'Выгоды по Трейд-ин до 300 000₽', subtitle: 'Только 20 автомобилей', link: 'order', icon: '' },
+    { id: 4, title: 'Новые SWM по сниженной цене от 1 395 000₽!', subtitle: 'Только 20 автомобилей', link: 'order', icon: '' },
 ]
 
 export function Actions() {
+    const { openModal } = useModal();
+
     return <div className="container">
         <div className={styles.actions}>
             <h2 className={`${styles.title} section-title`}>Акции</h2>
@@ -34,9 +45,9 @@ export function Actions() {
                             <div>
                                 {item.subtitle}
                             </div>
-                            <a href={item.link} target="_blank">
+                            <button onClick={() => openModal(item.link)}>
                                 Узнать подробнее
-                            </a>
+                            </button>
                         </div>
                     </SwiperSlide>
                 ))}
